68dbbfbbcf20868ef543699e8fdba3ed0acf72cb
tbfrogner <frogner1@hotmail.com>
2017-02-24 22:00:29 +0100

    Add files via upload

--- is_outside.m
+++ is_outside.m
@@ -9,7 +9,13 @@ if len > dist_to_p
     flag = false;
 else
     flag = true;
-    theta(1) = atan(p(2)/p(1));   
+    theta(1) = atan(p(2)/p(1));
+    if p(2)<0 && p(1)>0
+        theta(1)=theta(1)+pi;
+    end
+    if p(2)<0 && p(1)<0
+        theta(1)=theta(1)+pi;
+    end
 end
 end
     
