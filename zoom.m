dd81794bd95d8b4dbddb8cc45c46853b68a2c12e
Halvorsg <halvor.snersrud@gmail.com>
2017-02-24 16:14:08 +0100

    Nye funksjoner ZOOM og IS_OUTSIDE

--- zoom.m
+++ zoom.m
@@ -1,14 +1,20 @@
 function alpha = zoom(pk,theta,L,p)
+%Finner alpha som stemmer med wolfe betingelsene 
+%Sto på arket han skrev at man skulle øke ar til armijo holder, vet ikke om dette gjøre korrekt her. 
 d = @(theta,L,p) 1/2*norm([sum(L.*cos(cumsum(theta))),sum(L.*sin(cumsum(theta)))]-p)^2;
 dd = robot_gradient(theta,L,p);
 al = 0;
-ar = pi/2;
+ar = 1;
 %Vil øke ar til Armijo holder?
 c2 = 0.9;
 c1 = 10^-4;
 c1_dd_dot_pk = c1*dot(dd,pk);
 c2_df_dot_pk = c1_dd_dot_pk/c1*c2;
-alpha = pi/2;
+%burde være mulig å finne en bedre måte å finne ar på...
+while ~(d(theta+ar*pk,L,p) <= d(theta,L,p)+ar*c1_dd_dot_pk)
+    ar = ar+0.05;
+end
+alpha = (ar+al)/2;
 while ~(d(theta+alpha*pk,L,p) <= d(theta,L,p)+alpha*c1_dd_dot_pk && robot_gradient(theta+alpha*pk,L,p)'*pk >= c2_df_dot_pk)
     %while Wolfe conditiones not satisfied 
     if ~(d(theta+alpha*pk,L,p) <= d(theta,L,p)+alpha*c1_dd_dot_pk) %~(2. Wolfe cond.)
