cfa0531e3f368c7d87f34cfab120406abb019773
Halvorsg <halvor.snersrud@gmail.com>
2017-02-25 13:54:55 +0100

    find alpha

--- find_alpha.m
+++ find_alpha.m
@@ -1,9 +1,14 @@
 function alpha = find_alpha(pk,theta,L,p)
-%Finner alpha som stemmer med wolfe betingelsene 
-%Sto på arket han skrev at man skulle øke ar til armijo holder, vet ikke om dette gjøre korrekt her. 
+%% Preconditions
+%pk - Step direction
+%theta - vector with angels corresponding to the segments in L
+%L - vector with lengths og each segment
+%p - desired point
+%% Postconditions 
+%alpha - steplength fullfilling Wolfe condidions
+%% Initiating values
 d = @(theta,L,p) 1/2*norm([sum(L.*cos(cumsum(theta))),sum(L.*sin(cumsum(theta)))]-p)^2;
 dd = robot_gradient(theta,L,p);
-%% Initiating values
 al = 0;
 ar = 1;
 c2 = 0.9;
@@ -11,6 +16,7 @@ c1 = 10^-4;
 c1_dd_dot_pk = c1*dot(dd,pk);
 c2_df_dot_pk = c1_dd_dot_pk/c1*c2;
 alpha = 1;
+%% Finding alpha
 while ~(d(theta+alpha*pk,L,p) <= d(theta,L,p)+alpha*c1_dd_dot_pk && robot_gradient(theta+alpha*pk,L,p)'*pk >= c2_df_dot_pk)&& ar~=al
     %while Wolfe conditiones not satisfied 
     if ~(d(theta+alpha*pk,L,p) <= d(theta,L,p)+alpha*c1_dd_dot_pk) %~(2. Wolfe cond.)
